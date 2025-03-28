4ccbb37619cac945bfd52369cd01c60ae5984b4d
Aluno Dev25 <isabela.ferro@aluno.senai.br>
2025-03-28 09:23:47 -0300

    usuario e senha

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 import "fmt"
 
 func main() {
-var a,b int
-fmt.Println("Digite um numero para a")
-fmt.Scan(&a)
-fmt.Println("Digite um numero para b")
-fmt.Scan(&b)
-fmt.Println(" a soma de a e b é", a + b)
-fmt.Println("A subtração de a e b é", a - b)
-fmt.Println("A multiplicação entre a e b é", a * b)
-fmt.Println("a divisão entre a e b é", a/b)
+var usuario, senha string
+fmt.Println("Digite corretamente o usuario")
+fmt.Scan(&usuario)
+
+fmt.Println("Digite a senha")
+fmt.Scan(&senha)
 
 
-
+if usuario== "admin" && senha== "1234" {
+fmt.Println("Bem vindo")
+} else {
+fmt.Println("usuario e senha incorretos")
+}
 
 }
