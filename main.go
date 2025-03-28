84fd91cc82b1e871316cc1add8e0fa27cee493ba
Aluno Dev25 <isabela.ferro@aluno.senai.br>
2025-03-28 08:39:39 -0300

    Operações aritméticas desafio

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 import "fmt"
 
 func main() {
-	a+=1 
-	fmt.Println("Incrementar a", a)
+var a,b int
+fmt.Println("Digite um numero para a")
+fmt.Scan(&a)
+fmt.Println("Digite um numero para b")
+fmt.Scan(&b)
+fmt.Println(" a soma de a e b é", a + b)
+fmt.Println("A subtração de a e b é", a - b)
+fmt.Println("A multiplicação entre a e b é", a * b)
+fmt.Println("a divisão entre a e b é", a/b)
+
+
+
+
 }
