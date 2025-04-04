af6822230ca79f157e9f96d264e6726ca35773df
Aluno Dev25 <isabela.ferro@aluno.senai.br>
2025-04-04 08:56:27 -0300

    Biblioteca String e sort

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main 
-import "fmt"
-"strings"
-"sort"
+
+import (
+  "fmt"      
+  "sort"
+  "strings"
+  )
+
 func main() {
+  
   greeting := "hello my friends"
   fmt.Println(strings.Contains(greeting,"dogs"))
   fmt.Println(strings.ReplaceAll(greeting,"my", "mine"))
@@ -12,10 +17,28 @@ func main() {
   ages:= []int{50,80,10}
   sort.Ints(ages)
   fmt.Println(ages)
-  Index:= sort.SearchInts(ages,50)
+  index:= sort.SearchInts(ages,50)
 fmt.Println(index)
-names :=[]string{"Alice", "marco", "Diego"}
+names := []string{"Alice", "marco", "Diego"}
 sort.Strings(names)
 fmt.Println(names)
-fmt.Println(sort.SearchStrings(nomes,"Alice"))
+fmt.Println(sort.SearchStrings(names,"Alice"))
+x:=0
+for x < 5 {
+  fmt.Println(x)
+  x++
+  }
+  for i:=0; i <5; i++{
+    fmt.Println("for 2:")
+  }
+  for i:=0; i <len(names); i++{
+    fmt.Println(names)
+}
+
+for index, value := range names { 
+  fmt.Println("o indice é", index, "e o valor", value)
+}
+for index, value := range ages{
+fmt.Println("o índice é", index, "e o valor", value)
+}
 }
