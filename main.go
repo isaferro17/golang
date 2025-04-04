a4748b14a7dd784cf17f30c48d65e3176302a69e
Aluno Dev25 <isabela.ferro@aluno.senai.br>
2025-04-04 08:26:51 -0300

    Biblioteca String e sort

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main 
 import "fmt"
-
+"strings"
+"sort"
 func main() {
-// Slice
-var numeros = []int {2,4,8,10,12}
-fmt.Println(numeros)
-numeros[3] = 5
-fmt.Println(numeros, len(numeros), cap(numeros))
-numeros = append(numeros,14, 16, 18 )
-fmt.Println(numeros, len(numeros), cap(numeros))
+  greeting := "hello my friends"
+  fmt.Println(strings.Contains(greeting,"dogs"))
+  fmt.Println(strings.ReplaceAll(greeting,"my", "mine"))
+  fmt.Println(strings.ToUpper(greeting))
+  fmt.Println(strings.Index(greeting,"my"))
+  fmt.Println(strings.Split(greeting,""))
+  ages:= []int{50,80,10}
+  sort.Ints(ages)
+  fmt.Println(ages)
+  Index:= sort.SearchInts(ages,50)
+fmt.Println(index)
+names :=[]string{"Alice", "marco", "Diego"}
+sort.Strings(names)
+fmt.Println(names)
+fmt.Println(sort.SearchStrings(nomes,"Alice"))
 }
