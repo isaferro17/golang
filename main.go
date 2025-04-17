f25897a48906e5517ff16471fa2bc33ea217a371
Aluno Dev25 <isabela.ferro@aluno.senai.br>
2025-04-17 09:28:46 -0300

    idade e nome desafio

--- main.go
+++ main.go
@@ -1,29 +1,21 @@
 package main 
 
 import "fmt"      
-func dividir (dividendo int , divisor int) (int,string) {
-	if divisor == 0 {
-		return 0, "Erro na divisao por zero"
-}
-return dividendo / divisor , "sem erro"
-}
-func operacoesbasicas(a int, b int) (int, int,int) {
-	soma := a + b
-	multiplicacao := a * b
-	subtracao := a - b
-return soma, multiplicacao, subtracao
+func  dadospessoais() (name string,idade int) {
+
+fmt.Println("digite seu nome")
+fmt.Scan(&name)
+fmt.Println("Digite sua idade")
+fmt.Scan(&idade)
+return name, idade
 }
 
 func main(){
-resultado, erro := dividir (10,0)
-if erro != "sem erro" {
-fmt.Println(erro)
-} else {
-	fmt.Println("O resultado da divisão é",resultado, erro)
+var name, idade = dadospessoais()	
+if idade < 18 {
+ fmt.Println(name + ". Você é menor de idade")
 
+ } else{
+	fmt.Println(name + ". Você é maior de idade")
 }
-soma, multi, sub := operacoesbasicas(10,2)
-fmt.Println(soma)
-fmt.Println(multi)
-fmt.Println(sub)
 }
