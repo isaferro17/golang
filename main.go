9e42b782d31cb7aa32a27f6ddf81660e364b8491
Aluno Dev25 <isabela.ferro@aluno.senai.br>
2025-04-24 13:26:13 -0300

    Flash

--- main.go
+++ main.go
@@ -1,21 +1,12 @@
 package main 
 
 import "fmt"      
-func main(){
-alunoidade := make(map[string]int)
-alunoidade ["Bruno"] = 15
-alunoidade ["Otávio"] = 16 
-alunoidade ["fabiano"] = 40
-alunoidade ["Isabela"] = 15
-fmt.Println("idade da isabela", alunoidade,["Isabela"])
+func pegarnome() (string, string) {
+	return "Barry", "Allen"
+}
 
-notasalunos: map [string]float64{
-"Bruno" : 9,7,
-"Otávio" : 10,
-"fabiano" : 8,7,
-"Isabela" : 9,7,
-}
-for k, v := range notasalunos{
-fmt.Println("%s tirou a nota mais %f /n", k,v)
-}
+func main() {
+nome, sobrenome := pegarnome()
+fmt.Println(nome)
+fmt.Println(sobrenome)
 }
